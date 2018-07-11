822713edbbe40f7ba43e5c58024376bf1b1291a8
neil.tao83 <neil.tao@titansoft>
2018-07-12 00:16:05 +0800

    After feedback do some revise

--- mvc_HomeWork1/Controllers/MoneyController.cs
+++ mvc_HomeWork1/Controllers/MoneyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using mvc_HomeWork1.Models;
 using mvc_HomeWork1.Models.ViewModel;
 
 namespace mvc_HomeWork1.Controllers
@@ -12,8 +13,24 @@ namespace mvc_HomeWork1.Controllers
         // GET: Money
         public ActionResult Index()
         {
-            var moneyViewModel = new MoneyViewModel();
+            var moneyViewModel = new MoneyViewModel {Records = GetMoneyRecords()};
             return View(moneyViewModel);
+        }
+
+        private List<AccountRecord> GetMoneyRecords()
+        {
+            var accountRecords = new List<AccountRecord>();
+            for (int i = 0; i < 50; i++)
+            {
+                accountRecords.Add(new AccountRecord()
+                {
+                    Date = DateTime.Now.AddDays(-i),
+                    Money = i * 100,
+                    Type = i % 2 == 0 ? RecordType.Payout : RecordType.Income
+
+                });
+            }
+            return accountRecords;
         }
     }
 }
